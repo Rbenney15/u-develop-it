d6ec15ed3cdf5a4c5447c82e7336a80262be4920
Rob Benney <rob.benney@gmail.com>
2022-05-04 20:28:54 -0400

    create parties table

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: '',
+    password: 'u7q3*7bHZVmpuQZc@baP6bJmG',
     database: 'election'
   },
   console.log('Connected to the election database.')
