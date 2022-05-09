d68ccc855ec5a5ecca9119406ce4ef4ff3405bc1
Rob Benney <rob.benney@gmail.com>
2022-05-08 20:10:44 -0400

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+
+router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
-router.use(require('./candidateRoutes'));
 
 module.exports = router;
